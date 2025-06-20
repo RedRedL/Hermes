69bbbde34d0d86764bdfdcdfcd8a868df914e2ae
RedRedL <jaredajlim@proton.me>
2025-06-20 22:52:10 +1000

    Initialise

---
+++ src/main/java/red/webservertools/WebServerToolsClient.java
@@ -0,0 +1,10 @@
+package red.webservertools;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WebServerToolsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
